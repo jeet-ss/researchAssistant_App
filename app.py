769b36c59b360f039d4da87fa4b85f0dc7952d2d
jeet-ss <jeet.sensarma@gmail.com>
2024-11-15 17:04:31 +0100

    modularize code

--- app.py
+++ app.py
@@ -10,27 +10,7 @@ from utils.print_helpers import print_analysts
 from utils.sidebar import app_sidebar
 from utils.llms import instantiate_LLM_main
 from utils.webSearchTool import instantiate_webSearchTool_main
-
-
-explaination_text_header = """
-### What is it About?
-
-The **Research Assistant App** is an open-source project designed to streamline the research process. 
-It acts as a virtual research assistant, capable of conducting interviews, generating insightful questions, 
-and synthesizing information into comprehensive reports. Users can select and modify various personas and set 
-the maximum number of interviews they wish to conduct.
-
-For more details, visit:
-"""
-explaination_text_footer = """
-#### Key Features:
-- **Multiple AI Analyst Personas:** Tailor your research experience with multiple customizable AI personas using LangGraph.
-- **Parallel Web Search Capabilities:** Conduct multiple web searches simultaneously for efficient data gathering.
-- **User-Friendly Interface:** Navigate easily through the app for a seamless research experience.
-
-This tool is ideal for researchers, students, and professionals seeking efficient and organized data collection and analysis. 
-
-"""
+from models.streamlit_constants import explaination_text_header, explaination_text_footer
 
 
 def refresh_func(container):
---
+++ models/streamlit_constants.py
@@ -0,0 +1,19 @@
+explaination_text_header = """
+### What is it About?
+
+The **Research Assistant App** is an open-source project designed to streamline the research process. 
+It acts as a virtual research assistant, capable of conducting interviews, generating insightful questions, 
+and synthesizing information into comprehensive reports. Users can select and modify various personas and set 
+the maximum number of interviews they wish to conduct.
+
+For more details, visit:
+"""
+explaination_text_footer = """
+#### Key Features:
+- **Multiple AI Analyst Personas:** Tailor your research experience with multiple customizable AI personas using LangGraph.
+- **Parallel Web Search Capabilities:** Conduct multiple web searches simultaneously for efficient data gathering.
+- **User-Friendly Interface:** Navigate easily through the app for a seamless research experience.
+
+This tool is ideal for researchers, students, and professionals seeking efficient and organized data collection and analysis. 
+
+"""
